9dfd3ffb5c4e69e50cd8939fb6a7a3996b28cae5
Manith-2001 <manith.vazirani@pec.edu>
2026-09-02 08:50:22 +0300

    changed : sxo improvement for homepage

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Erroroverflow Blogs';
-export const SITE_DESCRIPTION = 'Helpful Blogs';
+export const SITE_DESCRIPTION =
+	'Practical debugging notes and guides on embedded systems and React Native development.';
 export const SITE_AUTHOR = 'Erroroverflow';
 export const SITE_AUTHOR_URL = 'https://blogs.erroroverflow.org/about/';
